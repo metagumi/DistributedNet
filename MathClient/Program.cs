4d6590bae4a8209ed49cfbe39b29f0d7f149c09c
metagumi <metagumi@gmail.com>
2018-07-01 18:42:08 -0400

    * .gitignore: first commit
    * SimpleMath.sln:
    * Program.cs:
    * SimpleMath.cs:
    * MathClient.csproj:
    * MathLibrary.csproj:
    * AssemblyInfo.cs:
    * AssemblyInfo.cs:

---
+++ MathClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MathClient
+{
+    class MathClient
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" 5 + 3 = {0}", 
+                              MathLibrary.SimpleMath.Add(5, 3));
+
+            Console.WriteLine(" 5 - 3 = {0}", 
+                              MathLibrary.SimpleMath.Subtract(5, 3));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathLibrary/SimpleMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace MathLibrary
+{
+    public class SimpleMath
+    {
+        public SimpleMath()
+        {
+        }
+
+        public static int Add(int n1, int n2) {
+            return n1 + n2;
+        }
+
+        public static int Subtract(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+
+    }
+}
